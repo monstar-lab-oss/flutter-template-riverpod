81e2267af96b8a61672daa01898c7d1223a59030
Diarmaid Lindsay <>
2022-12-05 16:07:42 +0900

    Use mock instead of dummy. Remove dependency on Riverpods to allow mocking.

--- lib/data/repository/my_anime_list_repository.dart
+++ lib/data/repository/my_anime_list_repository.dart
@@ -1,19 +1,17 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../api/api_client.dart';
 import '../model/api/request/get_top_anime_request.dart';
 import '../model/api/response/get_top_anime_response.dart';
-import '../providers/api_client_provider.dart';
 
 class MyAnimeListRepository {
   Future<GetTopAnimeResponse> getTopAnime({
-    required Ref ref,
+    required ApiClient apiClient,
     required GetTopAnimeRequest getTopAnimeRequest,
   }) async {
-    return ref.read(apiClientProvider).getTopAnime(
-          type: getTopAnimeRequest.type,
-          page: getTopAnimeRequest.page,
-          limit: getTopAnimeRequest.limit,
-          filter: getTopAnimeRequest.filter,
-        );
+    return apiClient.getTopAnime(
+      type: getTopAnimeRequest.type,
+      page: getTopAnimeRequest.page,
+      limit: getTopAnimeRequest.limit,
+      filter: getTopAnimeRequest.filter,
+    );
   }
 }
--- test/presentation/feature/detail/detail_page_test.dart
+++ test/presentation/feature/detail/detail_page_test.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template_riverpod/data/api/api_client.dart';
+import 'package:flutter_template_riverpod/data/model/api/response/get_top_anime_response.dart';
 import 'package:flutter_template_riverpod/data/providers/my_anime_list_repository_provider.dart';
+import 'package:flutter_template_riverpod/data/repository/my_anime_list_repository.dart';
 import 'package:flutter_template_riverpod/presentation/feature/news/top_anime_page.dart';
 import 'package:flutter_template_riverpod/presentation/feature/news/widgets/top_anime_item_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-import '../../../data/providers/my_anime_list_repository_provider_dummy.dart';
+import 'detail_page_test.mocks.dart';
 
 Finder _gridItemText(int id) =>
     find.byKey(ValueKey('GridTitleText_${id}title'));
+
+final _mockMyAnimeListRepository = MockMyAnimeListRepository();
 
 Future<void> _loadTopAnimePage(WidgetTester tester) async {
   await tester.pumpWidget(
     ProviderScope(
       overrides: [
         myAnimeListRepositoryProvider
-            .overrideWithValue(MyAnimeListRepositoryDummy())
+            .overrideWithValue(_mockMyAnimeListRepository)
       ],
       child: const MaterialApp(
         home: TopAnimePage(),
@@ -30,8 +37,31 @@ Future<void> _loadTopAnimePage(WidgetTester tester) async {
   expect(find.byType(CircularProgressIndicator), findsNothing);
 }
 
+@GenerateMocks([ApiClient, MyAnimeListRepository])
 void main() {
   testWidgets('Should display 2 Top Anime Widgets', (tester) async {
+    when(
+      _mockMyAnimeListRepository.getTopAnime(
+        apiClient: anyNamed('apiClient'),
+        getTopAnimeRequest: anyNamed('getTopAnimeRequest'),
+      ),
+    ).thenAnswer((_) async => const GetTopAnimeResponse(
+          data: [
+            GetTopAnimeResponseData(
+              malId: 1,
+              titleEnglish: 'Anime1',
+              titleJapanese: 'アニメ1',
+              images: null,
+            ),
+            GetTopAnimeResponseData(
+              malId: 2,
+              titleEnglish: 'Anime2',
+              titleJapanese: 'アニメ2',
+              images: null,
+            ),
+          ],
+        ));
+
     await _loadTopAnimePage(tester);
 
     expect(find.byType(TopAnimeItemWidget), findsNWidgets(2));
